a965b94db4b3790542233963919d89c01864e0da
Alexis Seigneurin <alexis@seigneurin.com>
2013-11-05 23:48:30 +0100

    - fixed the encoding - added support for republishing an ad: unpublish + publish - added support for accounts

--- src/main/java/com/seigneurin/adManager/Main.java
+++ src/main/java/com/seigneurin/adManager/Main.java
@@ -1,50 +1,48 @@
 package com.seigneurin.adManager;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FilenameFilter;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
 import org.junit.Assert;
 import org.yaml.snakeyaml.Yaml;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlFileInput;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlOption;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSelect;
-import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
+import java.io.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     private static Logger logger = Logger.getLogger("com.seigneurin.adManager");
 
+    private static String action;
     private static String pathname;
 
     private static SellerSettings sellerSettings;
     private static ObjectSettings objectSettings;
+    private static WebClient webClient;
 
     public static void main(String[] args) throws Exception {
         Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
 
         parseArguments(args);
         loadConfiguration();
+        if ("republish".equals(action))
+            deleteAd();
         postAd();
     }
 
     private static void parseArguments(String[] args) {
-        if (args.length != 1)
-            printUsageAndExit("Missing path.");
+        if (args.length < 2)
+            printUsageAndExit("Missing parameter.");
+        if (args.length > 2)
+            printUsageAndExit("Too many parameters.");
 
-        pathname = args[0];
+        action = args[0];
+        if ("publish".equals(action) == false && "republish".equals(action) == false)
+            printUsageAndExit("Invalid ACTION.");
+
+        pathname = args[1];
     }
 
     private static void printUsageAndExit(String reason) {
@@ -53,15 +51,18 @@ public class Main {
             System.out.println();
         }
 
-        System.out.println("Usage: PATH");
+        System.out.println("Usage: ACTION PATH");
+        System.out.println("  ACTION: publish, republish");
         System.out.println("  PATH: path to directory containing a settings.yaml file and a JPG photo.");
         System.exit(1);
     }
 
-    private static void postAd() throws Exception {
-        WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3);
+    private static void prepareWebClient() {
+        webClient = new WebClient(BrowserVersion.FIREFOX_3);
         webClient.setThrowExceptionOnScriptError(false);
+    }
 
+    private static HtmlPage loadAccountPage() throws IOException {
         // load the home page
 
         logger.log(Level.INFO, "Chargement de la page d'accueil...");
@@ -70,7 +71,7 @@ public class Main {
 
         // click the link to the region
 
-        logger.log(Level.INFO, "SÈlection de la rÈgion...");
+        logger.log(Level.INFO, "S√©lection de la r√©gion...");
         HtmlAnchor regionLink = homePage.getFirstAnchorByText(sellerSettings.region);
         Assert.assertNotNull(regionLink);
 
@@ -78,15 +79,38 @@ public class Main {
         Assert.assertNotNull(regionHomePage);
         Assert.assertEquals("Annonces " + sellerSettings.region + " - leboncoin.fr", regionHomePage.getTitleText());
 
+        // fill-in the login form
+
+        HtmlForm form = regionHomePage.getFormByName("loginform");
+
+        logger.log(Level.INFO, "Connexion au compte utilisateur...");
+
+        HtmlInput usernameElement = (HtmlInput) form.getInputByName("st_username");
+        usernameElement.setValueAttribute(sellerSettings.email);
+
+        HtmlInput passwordElement = (HtmlInput) form.getInputByName("st_passwd");
+        passwordElement.setValueAttribute(sellerSettings.password);
+
+        HtmlInput okButton = form.getFirstByXPath("//input[@type='submit']");
+        HtmlPage accountPage = okButton.click();
+        Assert.assertNotNull(accountPage);
+        Assert.assertEquals("Compte", accountPage.getTitleText());
+        return accountPage;
+    }
+
+    private static void postAd() throws Exception {
+        prepareWebClient();
+        HtmlPage accountPage = loadAccountPage();
+
         // click the link to the page to post an ad
 
-        logger.log(Level.INFO, "Chargement de la page de dÈpÙt d'annonces...");
-        HtmlAnchor postAdLink = regionHomePage.getFirstAnchorByText("DÈposer une annonce");
+        logger.log(Level.INFO, "Chargement de la page de d√©p√¥t d'annonces...");
+        HtmlAnchor postAdLink = accountPage.getFirstAnchorByText("D√©poser une annonce");
         Assert.assertNotNull(postAdLink);
 
         HtmlPage postAdPage = postAdLink.click();
         Assert.assertNotNull(postAdPage);
-        Assert.assertEquals("Formulaire de dÈpÙt de petites annonces gratuites sur Leboncoin.fr",
+        Assert.assertEquals("Formulaire de d√©p√¥t de petites annonces gratuites sur Leboncoin.fr",
                 postAdPage.getTitleText());
 
         // fill-in the form
@@ -104,7 +128,7 @@ public class Main {
         HtmlInput zipCodeElement = (HtmlInput) form.getInputByName("zipcode");
         zipCodeElement.setValueAttribute(sellerSettings.zipCode);
 
-        logger.log(Level.INFO, "Remplissage des champs 'CatÈgorie'...");
+        logger.log(Level.INFO, "Remplissage des champs 'Cat√©gorie'...");
 
         HtmlSelect categoryElement = form.getSelectByName("category");
         selectOption(categoryElement, objectSettings.category);
@@ -154,31 +178,62 @@ public class Main {
                 System.err.println("Error: " + errorElement.asText());
         }
 
-        Assert.assertEquals("VÈrifiez votre annonce.", validationPage.getTitleText());
+        Assert.assertEquals("V√©rifiez votre annonce.", validationPage.getTitleText());
 
         HtmlElement photoElement = validationPage.getFirstByXPath("//div[text()='Photo principale']");
         Assert.assertNotNull("Photo was not uploaded", photoElement);
 
         form = validationPage.getFormByName("formular");
 
-        logger.log(Level.INFO, "Remplissage des champs 'VÈrifiez le contenu de votre annonce'...");
+        logger.log(Level.INFO, "Remplissage des champs 'V√©rifiez le contenu de votre annonce'...");
 
         HtmlSelect cityElement = form.getSelectByName("city");
         selectOption(cityElement, sellerSettings.city);
 
-        HtmlInput passwordElement = form.getInputByName("passwd");
-        passwordElement.setValueAttribute(sellerSettings.password);
-
-        HtmlInput passwordVerElement = form.getInputByName("passwd_ver");
-        passwordVerElement.setValueAttribute(sellerSettings.password);
+        HtmlCheckBoxInput acceptRuleCheckBox = form.getInputByName("accept_rule");
+        acceptRuleCheckBox.setChecked(true);
 
         HtmlInput createButton = form.getInputByName("create");
         HtmlPage creationPage = createButton.click();
 
-        logger.log(Level.INFO, "TerminÈ !");
+        logger.log(Level.INFO, "Termin√© !");
 
-        HtmlElement mainTextElement = creationPage.getFirstByXPath("//div[@class='maintext']");
-        System.out.println(mainTextElement.asText());
+        Assert.assertEquals("Confirmation d'envoi de votre annonce", creationPage.getTitleText());
+        Assert.assertTrue(creationPage.asText().contains("Votre annonce a √©t√© envoy√©e √† notre √©quipe √©ditoriale."));
+    }
+
+    private static void deleteAd() throws Exception {
+        prepareWebClient();
+        HtmlPage accountPage = loadAccountPage();
+
+        logger.log(Level.INFO, "Chargement de la page de l'objet...");
+
+        HtmlAnchor objectLink = accountPage.getFirstAnchorByText(objectSettings.subject);
+        HtmlPage objectPage = objectLink.click();
+        Assert.assertNotNull(objectPage);
+        Assert.assertTrue("Unexpected page: " + objectPage.getTitleText(),
+                objectPage.getTitleText().startsWith(objectSettings.subject));
+
+        logger.log(Level.INFO, "Chargement de la page de suppression de l'objet...");
+
+        HtmlAnchor deleteLink = objectPage.getFirstAnchorByText("Supprimer");
+        HtmlPage deleteObjectPage = webClient.getPage(deleteLink.getHrefAttribute());
+        //HtmlPage deleteObjectPage = deleteLink.click();
+        Assert.assertNotNull(deleteObjectPage);
+        Assert.assertEquals("Leboncoin.fr - Gestion de l'annonce", deleteObjectPage.getTitleText());
+
+        logger.log(Level.INFO, "Suppression de l'objet...");
+
+        HtmlInput continueButton = deleteObjectPage.getFirstByXPath("//input[@id='store__continue']");
+        HtmlPage deletionPage = continueButton.click();
+        Assert.assertEquals("Confirmation de votre suppression", deletionPage.getTitleText());
+
+        logger.log(Level.INFO, "Validation...");
+
+        HtmlInput validateButton = (HtmlInput) deletionPage.getElementById("st_ads_continue");
+        HtmlPage endPage = validateButton.click();
+        Assert.assertTrue(endPage.asText().contains("Votre demande de suppression"));
+        Assert.assertTrue(endPage.asText().contains("bien √©t√© prise en compte."));
     }
 
     private static void loadConfiguration() throws FileNotFoundException {
@@ -187,12 +242,12 @@ public class Main {
         File path = new File(pathname);
 
         String sellerYamlFilename = path.getParent() + File.separator + "settings.yaml";
-        logger.log(Level.INFO, "Chargement des propriÈtÈs du vendeur : " + sellerYamlFilename);
+        logger.log(Level.INFO, "Chargement des propri√©t√©s du vendeur : " + sellerYamlFilename);
         FileInputStream mainYamlFileStream = new FileInputStream(sellerYamlFilename);
         sellerSettings = yaml.loadAs(mainYamlFileStream, SellerSettings.class);
 
         String objectYamlFilename = path + File.separator + "settings.yaml";
-        logger.log(Level.INFO, "Chargement des propriÈtÈs de l'objet : " + objectYamlFilename);
+        logger.log(Level.INFO, "Chargement des propri√©t√©s de l'objet : " + objectYamlFilename);
         FileInputStream objectYamlFileStream = new FileInputStream(objectYamlFilename);
         objectSettings = yaml.loadAs(objectYamlFileStream, ObjectSettings.class);
 
